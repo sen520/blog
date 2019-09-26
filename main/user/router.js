ed246e4033728b91429251939150a101ccf14466
senzhang <358974225@qq.com>
2019-09-26 14:50:26 +0800

    add update userinfo

--- main/user/router.js
+++ main/user/router.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')();
 const { login } = require('./login');
 const { register } = require('./register');
+const { updateUserInfo } = require('./userInfo');
 
 router.prefix('/users');
 
@@ -10,6 +11,10 @@ router.post('/login', async (ctx) => {
 
 router.post('/register', async (ctx) => {
   await register(ctx.request.body, ctx);
+});
+
+router.post('/update', async (ctx) => {
+  await updateUserInfo(ctx.request.body, ctx);
 });
 
 module.exports = router;
---
+++ main/user/userInfo.js
@@ -0,0 +1,32 @@
+const { UserSchema } = require('../static/schema');
+const { setError, setResult } = require('../static/utility');
+
+/**
+ * update user info
+ *
+ * @param info
+ * @param ctx
+ * @returns {Promise<void>}
+ */
+/* eslint-disable no-underscore-dangle, no-param-reassign */
+async function updateUserInfo(info, ctx) {
+  try {
+    const userId = info._id;
+    const result = await UserSchema.findOne({ _id: userId });
+    if (!result) {
+      setResult(ctx, 404, 'no such user');
+      return;
+    }
+    delete info._id;
+    delete info.password;
+    console.log(info);
+    const user = await UserSchema.update({ _id: userId }, { $set: info });
+    setResult(ctx, 200, user);
+  } catch (e) {
+    setError(ctx, e);
+  }
+}
+
+module.exports = {
+  updateUserInfo,
+};
