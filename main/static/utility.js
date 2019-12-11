5a22dd1646765e770a32a1ad02859077453bde42
senzhang <358974225@qq.com>
2019-12-11 14:06:09 +0800

    get ip

--- main/static/utility.js
+++ main/static/utility.js
@@ -22,8 +22,21 @@ const formatUrlParams = (params) => {
   return encodeURIComponent(JSON.stringify(params));
 };
 
+/**
+ * @getClientIP
+ * @desc 获取用户 ip 地址
+ * @param {Object} req - 请求
+ */
+function getClientIP(req) {
+  return req.headers['x-forwarded-for'] || // 判断是否有反向代理 IP
+      req.connection.remoteAddress || // 判断 connection 的远程 IP
+      req.socket.remoteAddress || // 判断后端的 socket 的 IP
+      req.connection.socket.remoteAddress;
+}
+
 module.exports = {
   setResult,
   setError,
   formatUrlParams,
+  getClientIP,
 };
